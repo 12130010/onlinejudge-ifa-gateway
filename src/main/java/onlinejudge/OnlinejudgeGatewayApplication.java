da94ccbc9d76268bf90fd86e3efea04ec4041e6b
Hoang Nhuoc Quy <hoangnhuocquy0@gmail.com>
2017-02-02 10:20:16 +0700

    ITO-000 create project

---
+++ src/main/java/onlinejudge/OnlinejudgeGatewayApplication.java
@@ -0,0 +1,16 @@
+package onlinejudge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+@EnableZuulProxy
+public class OnlinejudgeGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OnlinejudgeGatewayApplication.class, args);
+	}
+}
